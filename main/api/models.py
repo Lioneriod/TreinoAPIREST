b9750a98faebb6ae66fe0cc5713a89903b3230cc
alunoUnifor <alunoUnifor@edu.unifor.br>
2025-09-24 20:02:05 -0300

    try to make this work(?)

--- main/api/models.py
+++ main/api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    ingrediente = models.CharField()
+    receita = models.CharField()
--- main/api/views.py
+++ main/api/views.py
@@ -1,3 +1,9 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from django.shortcuts import render
-
 # Create your views here.
+
+@api_view(['GET'])
+def getData(request):
+    person = {'name':'leonardo','age':27}
+    return Response(person)
